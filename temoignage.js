b7192bdce56c2cf1b89c8eb5012f655eeef827b5
william.biencourt@live.fr <william.biencourt@live.fr>
2023-05-15 15:29:43 +0200

    update_william

--- temoignage.js
+++ temoignage.js
@@ -1,37 +1,3 @@
-function temoignagestxt(img, titre, text) {
-
-    return `
-    <div class="temoignageitem">
-    <img src="img/${img}" class="temoignagebackground">
-    <div class="temoignageabso">
-        <div class="temoignageitemtitre">
-            ${titre}
-        </div>
-        <div class="temoignageitemtext">
-            ${text}
-        </div>
-    </div>
-    <a href="" class="temoignagesfavoris">
-        <img src="img/heart.png" class="temoignagesfavoris">
-    </a>
-</div>
-    `;
-  }
-  
-
-  let temoignages = '';
-
-  //dans l'ordre ("img, titre, texte")
-  temoignages += temoignagestxt("temoignage1.jpg","J'ai fait beaucoup de velo holala j'ai mal au pieds","Je pedalle, tu pedalle, tout le monde ils pedalent!");
-  temoignages += temoignagestxt("temoignage1.jpg","J'ai fait beaucoup de velo holala j'ai mal au pieds","Je pedalle, tu pedalle, tout le monde ils pedalent!");
-  temoignages += temoignagestxt("temoignage1.jpg","J'ai fait beaucoup de velo holala j'ai mal au pieds","Je pedalle, tu pedalle, tout le monde ils pedalent!");
-  temoignages += temoignagestxt("temoignage1.jpg","J'ai fait beaucoup de velo holala j'ai mal au pieds","Je pedalle, tu pedalle, tout le monde ils pedalent!");
-  document.getElementById("temoignages").innerHTML = temoignages;
-
-
-
-
-
   function trouvevelotxt(lien, img, text) {
 
     return `
