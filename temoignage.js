030658fa9eb1be942a71d709403df0516de8f06f
Mazurier <antoinemazuriermail@gmail.com>
2023-05-15 11:41:27 +0200

    Trouve velo

--- temoignage.js
+++ temoignage.js
@@ -1,7 +1,7 @@
 function temoignagestxt(img, titre, text) {
 
     return `
-    <div class="temoignageitem" style=" background-image: url(img/${img});">
+    <div class="temoignageitem" style="background-image: url(img/${img});">
     <div>
         <div class="temoignageitemtitre">
             ${titre}
@@ -23,4 +23,34 @@ function temoignagestxt(img, titre, text) {
   temoignages += temoignagestxt("temoignage1.jpg","J'ai fait beaucoup de velo holala j'ai mal au pieds","Je pedalle, tu pedalle, tout le monde ils pedalent!");
   temoignages += temoignagestxt("temoignage1.jpg","J'ai fait beaucoup de velo holala j'ai mal au pieds","Je pedalle, tu pedalle, tout le monde ils pedalent!");
   document.getElementById("temoignages").innerHTML = temoignages;
+
+
+
+
+
+  function trouvevelotxt(lien, img, text) {
+
+    return `
+    <a href="${lien}" class="nodeco">
+    <div class="trouveveloitem">
+        <img src="img/${img}" class="trouvebackground">
+        <div class="trouvetxtimg">
+        ${text}
+        </div>
+    </div>
+    </a>
+    `;
+  }
+  
+
+  let trouvevelo = '';
+
+  //dans l'ordre ("img, texte")
+  trouvevelo += trouvevelotxt("","trouve1.png", "HÉBERGEMENTS");
+  trouvevelo += trouvevelotxt("","trouve2.png", "LIEUX DE VISITE");
+  trouvevelo += trouvevelotxt("","trouve3.png", "OFFICES DE TOURISME");
+  trouvevelo += trouvevelotxt("","trouve4.png", "LOUEURS & RÉPARATEURS DE VÉLO");
+  document.getElementById("trouvevelo").innerHTML = trouvevelo;
+
+
   
