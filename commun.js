2f836a092c20121e965f349f09741482cd195750
Mazurier <antoinemazuriermail@gmail.com>
2023-05-15 16:27:15 +0200

    Burgermenu phone

--- commun.js
+++ commun.js
@@ -82,32 +82,59 @@ return `
 document.getElementById("header").innerHTML=header();
 
 
-/*
-var navbarvar='';
 
-function navbar(){
+var navvar='';
+
+function nav(){
 
 return `
+<img src="img/menu2.png" alt="" class="navbaricon" onclick="navbar()">
+        <div class="navbarmain">
+            <div class="navbarlinks">
+                <a href="index.html">Accueil</a>
+                <a href="">L'itinéraire</a>
+                <a href="">Témoignages</a>
+                <a href="">Infos Pratiques</a>
+                <a href="">Suggestions</a>
+                <a href="">Inspirations</a>
+                <a href="">Actualités</a>
+                <a href="">PASSEPORT</a>
+            </div>
+            <div class="navbartrait"></div>
+            <div class="navbarlinkfin">
+                Espace pro
+            </div>
+            <div class="navbartrait"></div>
+            <div class="navbarlinkfin">
+                Carnet de voyage
+            </div>
+            <div class="navbartrait"></div>
+            <div class="navbarlinkfin">
+                Espace personel
+            </div>
+            <div class="navbartrait"></div>
+            <a href=""><span class="navbarnews">S'abonner a la newsletter</span></a>
+        </div>
 
 `
 
 }
 
-navbarvar = navbarvar + navbar() 
 
-document.getElementById("navbar").innerHTML=navbarvar
-
+document.getElementById("navbar").innerHTML=nav();
 
 
+let headernav = '';
 
-/*
+function headernavbar() {
+    return `
+    <div id="navlink">
+            <img src="img/menu.png" class="burgermenu" onclick="navbar()">
+            
+        </div>
+        <img src="img/logoheader.png" alt="" class="logoheader">
+            <img src="img/search.png" alt="" class="headersearchicon">
+    `
+};
 
-function navbar() {
-    var x = document.getElementById("links");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-  */
+document.getElementById("headerphone").innerHTML=headernavbar();
