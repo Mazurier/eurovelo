881df7f29f956b82fc7f004716f69db6b1d86827
Mazurier <antoinemazuriermail@gmail.com>
2023-05-16 15:11:23 +0200

    GPX sur carte + bricollage retire pins

--- map.js
+++ map.js
@@ -4,37 +4,37 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="http://www.osm.org">OpenStreetMap</a'}).addTo(map);
 
 var gpx0 ='gpx/calais-ardres.gpx';
-new L.GPX(gpx0, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpx0, {async:true}).addTo(map);
 
 var gpx1 ='gpx/ardres-watten.gpx';
-new L.GPX(gpx1, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpx1, {async:true}).addTo(map);
 
 var gpx2 ='gpx/watten-st-omer.gpx';
-new L.GPX(gpx2, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpx2, {async:true}).addTo(map);
 
 var gpx3 ='gpx/st-omer-aire-sur-la-lys.gpx';
-new L.GPX(gpx3, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpx3, {async:true}).addTo(map);
 
 var gpx4 ='gpx/aire-sur-la-lys-st-venant.gpx';
-new L.GPX(gpx4, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpx4, {async:true}).addTo(map);
 
 var gpx5 ='gpx/st-venant-bethune.gpx';
-new L.GPX(gpx5, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpx5, {async:true}).addTo(map);
 
 var gpx6 ='gpx/bethunes-olhain.gpx';
-new L.GPX(gpx6, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpx6, {async:true}).addTo(map);
 
 var gpx7 ='gpx/olhain-angres.gpx';
-new L.GPX(gpx7, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpx7, {async:true}).addTo(map);
 
 var gpx8 ='gpx/angres-lens.gpx';
-new L.GPX(gpx8, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpx8, {async:true}).addTo(map);
 
 var gpx9 ='gpx/lens-don.gpx';
-new L.GPX(gpx9, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpx9, {async:true}).addTo(map);
 
 var gpxa ='gpx/don-lille.gpx';
-new L.GPX(gpxa, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpxa, {async:true}).addTo(map);
 
 var gpxb ='gpx/lille-wattrelos.gpx';
-new L.GPX(gpxb, {async:true}).on('loaded').addTo(map);
+new L.GPX(gpxb, {async:true}).addTo(map);
